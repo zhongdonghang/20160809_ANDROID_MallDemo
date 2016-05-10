9dc2bd3d9817c058a1fc4d5b78510766e1634986
Mnzi2015 <260706998@qq.com>
2016-05-10 12:06:29 +0800

    修改 商品详细页

--- app/src/main/java/com/luoyp/brnmall/activity/PayActivity.java
+++ app/src/main/java/com/luoyp/brnmall/activity/PayActivity.java
@@ -12,7 +12,6 @@ import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.alipay.sdk.app.PayTask;
 import com.luoyp.brnmall.BaseActivity;
 import com.luoyp.brnmall.R;
 import com.luoyp.brnmall.alipay.PayResult;
